f862c1aba2e644232eec5ad504c75cf5ccc15bea
Daniel Hall <daniel@danielhall.me>
2013-11-27 00:22:45 +1100

    Takes arguments now

--- listen.py
+++ listen.py
@@ -2,6 +2,7 @@
 
 import mosquitto
 import json
+import argparse
 import Image
 from StringIO import StringIO
 import wx
@@ -66,13 +67,25 @@ def on_message(mosq, obj, msg):
     img = Image.open(StringIO(imgdata))
     ui.newFrame(img)
 
+parser = argparse.ArgumentParser(description='Displays MQTTitude on a map')
+parser.add_argument('-u', '--username', help='Username for the MQTT server')
+parser.add_argument('-P', '--password', help='Password for the MQTT server')
+parser.add_argument('-c', '--cafile', help='The CA certificate for the MQTT server')
+parser.add_argument('-H', '--host', default='127.0.0.1', help='The hostname of the MQTT server')
+parser.add_argument('-p', '--port', default=1883, type=int, help='The port of the MQTT server')
+args = vars(parser.parse_args())
+
 ui = UIThread()
 ui.start()
 
-client = mosquitto.Mosquitto("test-client")
-client.tls_set('ca.crt')
-client.username_pw_set('testing', 'c1rvyiZhoDgn')
-client.connect("mqtt.tildaslash.com")
+client = mosquitto.Mosquitto("stalkitude")
+
+if args['cafile'] is not None:
+    client.tls_set(args['cafile'])
+
+if args['username'] is not None:
+    client.username_pw_set(args['username'], args['password'])
+client.connect(args['host'], port=args['port'])
 
 client.on_message = on_message
 
